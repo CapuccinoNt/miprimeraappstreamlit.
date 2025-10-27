ca0436f12eeb5e96d74af76b9ae7e60ab9adc04f
CapuccinoNt <111159762+CapuccinoNt@users.noreply.github.com>
2025-10-27 13:29:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
 # Título de la app
-st.title("¡Mi primera app!")
+st.title("Adivinare Cuantos años tienes!")
 
 # Texto simple
-st.write("Hola, soy [TU NOMBRE] y esta es mi primera aplicación con Streamlit.")
+st.write("Hola, soy akinator y adivinare tu nombre")
 
 # Un input interactivo
 nombre = st.text_input("¿Cómo te llamas?")
 
 # Respuesta condicional
 if nombre:
-    st.write(f"¡Hola, {nombre}! Bienvenido/a a mi app")
+    st.write(f"...Te llamas: {nombre}! ¿Acerté?")
 
 # Un botón
 if st.button("Presiona aquí"):
