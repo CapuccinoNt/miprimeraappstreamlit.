1e03ef968e0307ef6ee2feffbe93844441aa913b
CapuccinoNt <111159762+CapuccinoNt@users.noreply.github.com>
2025-10-27 13:30:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
 # Título de la app
-st.title("Adivinare Cuantos años tienes!")
+st.title("Te dire tu edad")
 
 # Texto simple
-st.write("Hola, soy akinator y adivinare tu nombre")
+st.write("Hola, te dire tu edad EXACTA")
 
 # Un input interactivo
-nombre = st.text_input("¿Cómo te llamas?")
+nombre = st.text_input("¿Cuantos años tienes?")
 
 # Respuesta condicional
 if nombre:
-    st.write(f"...Te llamas: {nombre}! ¿Acerté?")
+    st.write(f"tienes {nombre} años")
 
 # Un botón
 if st.button("Presiona aquí"):
